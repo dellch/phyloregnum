caf8cbd750030884aa25f540b9cc5706faac42de
Greg Traub <gtraub@Gregs-MacBook-Pro.local>
2015-01-01 18:42:34 -0500

    adding files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Regnum::Application.config.secret_token = '30d60a01a8f09b4cc74503173a2bfe8ec105ff29445b16adb4b18aa288dfde0c0a849d2af67f2a557f6b143062f4a0da7701b17c9074a982edee17eaf3737892'
